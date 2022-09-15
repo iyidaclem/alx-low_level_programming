448b7e338faf3cd56a16cadcd072fe146b6fac2d
iyidaclem <iyidaclem@gmail.com>
2022-09-15 17:20:22 +0100

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,16 +18,14 @@ int main(void)
 	while (++i < 101)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz");
+			printf(" Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz");
+			printf(" Buzz");
 		else
-			printf("%d", i);
-		if (i == 100)
-			printf("\n");
-		else
-			printf(" ");
+			printf(" %d", i);
 	}
+	printf("\n");
+	return (0);
 }
