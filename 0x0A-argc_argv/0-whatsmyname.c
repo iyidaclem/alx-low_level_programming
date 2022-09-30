f2dbf698a8290a1eb7728e67a35c3ed9175f96fa
iyidaclem <iyidaclem@gmail.com>
2022-09-30 23:07:17 +0100

    Whatismyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,12 @@
 *main - program that prints its name
 *@argc: Argument count
 *@argv: array of arguments
+*Return: always succcess (0)
 */
 
 int  main(int argc __attribute__((unused)), char **argv)
 {
 	printf("%s\n", argv[0]);
+	return (0);
 }
 
