4c211e3444a1942aa53357d455b76b687587b41b
iyidaclem <iyidaclem@gmail.com>
2022-10-12 18:23:59 +0100

    program that performs simple operations.

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,65 +1,71 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
-*op_add - calculate the sum of two integers  a and b
-*@a: first integer
-*@b: second integer
-*Return: the sum of a and b
-*/
-
+ * op_add - adds two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: add.
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
-
 /**
-*op_sub- calculate the difference of two integers  a and b
-*@a: first integer
-*@b: second integer
-*Return: the difference of a and b
-*/
-
+ * op_sub - subctracts two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: difference.
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-/**
-*op_mul - calculate the product of two integers  a and b
-*@a: first integer
-*@b: second integer
-*Return: the product of a and b
-*/
 
+/**
+ * op_mul - multiplies two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: multiplication.
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
-
 /**
-*op_div- calculate the sum of qoutient integers  a and b
-*@a: first integer
-*@b: second integer
-*Return: the qoutient of a by b
-*/
-
+ * op_div - divides two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: division.
+ */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
-
 /**
-*op_mod - alculate the modulus of two integers  a and b
-*@a: first integer
-*@b: second integer
-*Return: the modulus of a and b
-*/
-
+ * op_mod - calculates the module of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: remainder of the division.
+ */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
