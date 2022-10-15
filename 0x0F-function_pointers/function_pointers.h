255bc308fbbfce315aeb4fe7b4f2e36c5125dd2d
iyidaclem <iyidaclem@gmail.com>
2022-10-15 19:20:50 +0100

    print_name corrected

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef _main_h_
 #define _main_h_
-
+#include <stddef.h>
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
