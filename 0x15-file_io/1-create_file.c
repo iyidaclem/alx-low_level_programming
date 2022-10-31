a6d150cdaaba886539504b978f19557926c30438
iyidaclem <iyidaclem@gmail.com>
2022-10-31 18:16:10 +0100

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		i = 1;
-	fd = open(filename, O_TRUNC | O_RDWR | O_CREAT, 0600);
+	fd = open(filename, O_TRUNC | O_WRONLY | O_CREAT, 0600);
 
 	if (fd == -1)
 		i = -1;
